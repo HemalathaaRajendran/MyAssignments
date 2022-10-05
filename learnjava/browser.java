75ad8782a5a75e42f846685d076a15d1a28189ff
HemalathaRajendran <114945481+HemalathaaRajendran@users.noreply.github.com>
2022-10-05 16:50:47 +0530

    Add files via upload

---
+++ learnjava/browser.java
@@ -0,0 +1,10 @@
+package learnjava;
+
+public class browser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("This is my Browser");
+	}
+
+}
